6759507abcfd88282fd02cdbf0f0547d6c12d853
adammcclenaghan <adam@mcclenaghan.co.uk>
2021-08-10 01:12:36 +0100

    Dont need maven compile anymore

--- build-run.sh
+++ build-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mvn clean install && docker-compose down --rmi all && docker-compose up 
+docker-compose down --rmi all && docker-compose up 
 #mvn clean install && docker-compose up --force-recreate
