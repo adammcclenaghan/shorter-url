7fd47c1531c6bdb4ad34c0b7601144f02ada5341
adammcclenaghan <adam@mcclenaghan.co.uk>
2021-08-10 14:27:40 +0100

    Remove some files

--- del-docker.sh
+++
@@ -1,2 +0,0 @@
-docker rm -vf $(docker ps -a -q) && docker rmi -f $(docker images -a -q); docker volume rm $(docker volume ls -q)
-
--- dev-test.sh
+++
@@ -1,5 +0,0 @@
-curl http://localhost:8080/shortUrls
-
-curl -i -X POST -H "Content-Type:application/json" -d "{\"longUrl\": \"hello.com\"}" http://localhost:8080/shortUrls
-
-curl http://localhost:8080/shortUrls
